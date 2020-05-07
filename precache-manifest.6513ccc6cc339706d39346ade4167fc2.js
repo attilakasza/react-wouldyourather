e1a78b7e90fdb11df69543623e1623e929870e61
Attila Kasza <9ttila.kasza@gmail.com>
2020-05-07 14:09:33 +0200

    Updates

---
+++ precache-manifest.6513ccc6cc339706d39346ade4167fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5ceff095c3099968d831200717d651",
+    "url": "/react-wouldyourather/index.html"
+  },
+  {
+    "revision": "bc870c31e8413fbbfef8",
+    "url": "/react-wouldyourather/static/css/main.3a880779.chunk.css"
+  },
+  {
+    "revision": "ea3008e982412c7c1c13",
+    "url": "/react-wouldyourather/static/js/2.778670d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-wouldyourather/static/js/2.778670d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc870c31e8413fbbfef8",
+    "url": "/react-wouldyourather/static/js/main.5f8f88d3.chunk.js"
+  },
+  {
+    "revision": "24364ed7021825372e7d",
+    "url": "/react-wouldyourather/static/js/runtime-main.5028d86d.js"
+  }
+]);
